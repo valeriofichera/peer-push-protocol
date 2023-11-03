cd3f29e3dac073d679d248dbcbc694f7f875d3ca
Markeljan Sokoli <Markeljan19@gmail.com>
2023-11-03 23:16:04 +0000

    clean up example

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -38,85 +38,9 @@ type ExpectedLog = {
   transactionHash: `0x${string}`;
 } & Log
 
-// Helper function to sanitize twitter handle
-const sanitizeHandle = (twitterHandleInput: string): string => {
-  let handle = twitterHandleInput.replace(/\s/g, '');
-  if (handle.startsWith('https://twitter.com/')) handle = handle.replace('https://twitter.com/', '');
-  else if (handle.startsWith('twitter.com/')) handle = handle.replace('twitter.com/', '');
-  if (!handle.startsWith('@')) handle = '@' + handle;
-  if (!/^@[a-zA-Z0-9_]{1,15}$/.test(handle)) handle = '';
-  return handle;
-}
 
 // Function to handle ChallengeSolved event
-const handleChallengeSolvedEvent = async (twitterHandleInput: string, challenge: `0x${string}`, transactionHash: `0x${string}`) => {
+const handleEventTriggered = async (twitterHandleInput: string, challenge: `0x${string}`, transactionHash: `0x${string}`) => {
 }
-
-// Function to get token id
-const getTokenId = async (transactionHash: `0x${string}`): Promise<number> => {
-  return 0;
-  // const transactionReceipt: TransactionReceipt = await publicClient.getTransactionReceipt({
-  //   hash: transactionHash,
-  // });
-  // const transferTopics = encodeEventTopics({
-  //   abi: FOUNDRY_COURSE_CONTRACT_ABI,
-  //   eventName: 'Transfer',
-  //   args: {
-  //     from: '0x0000000000000000000000000000000000000000',
-  //   }
-  // });
-
-  // let tokenId;
-  // for (let i = 0; i < transactionReceipt.logs.length; i++) {
-  //   if (transactionReceipt.logs[i]['topics'][0] !== transferTopics[0] || transactionReceipt.logs[i]['topics'][1] !== transferTopics[1]) {
-  //     continue;
-  //   }
-
-  //   const decodedEventLog = decodeEventLog({
-  //     abi: FOUNDRY_COURSE_CONTRACT_ABI,
-  //     eventName: 'Transfer',
-  //     topics: transactionReceipt.logs[i]['topics'],
-  //     data: transactionReceipt.logs[i].data,
-  //   });
-
-  //   tokenId = fromHex(decodedEventLog.args['tokenId'], 'number');
-  //   break;
-  // }
-
-  // if (!tokenId) {
-  //   console.log('No matching Transfer event found in the transaction receipt.');
-  //   return;
-  // }
-
-  // return tokenId;
-};
-
-// Function to get challenge attribute
-// const getChallengeAttribute = async (tokenId: number): Promise<string> => {
-//   const tokenUri = await publicClient.readContract({
-//     abi: FOUNDRY_COURSE_CONTRACT_ABI,
-//     address: FOUNDRY_COURSE_CONTRACT_ADDRESS,
-//     functionName: 'tokenURI',
-//     args: [tokenId],
-//   });
-
-//   const parsedUri = tokenUri.toString().split("base64,")[1];
-//   const jsonString = Buffer.from(parsedUri, 'base64').toString('utf-8');
-//   const jsonData = JSON.parse(jsonString);
-//   const challengeAttribute = jsonData?.attributes[0]?.trait_type;
-//   return challengeAttribute;
-// };
-
-// Watch for contract event
-// webSocketClient.watchContractEvent({
-//   address: FOUNDRY_COURSE_CONTRACT_ADDRESS,
-//   abi: FOUNDRY_COURSE_CONTRACT_ABI,
-//   eventName: 'ChallengeSolved',
-//   onLogs: (logs: ExpectedLog[]) => {
-//     console.log("New Challenge Solved Event!", logs);
-//     const { args: { solver, challenge, twitterHandle }, eventName, transactionHash } = logs[0];
-//     handleChallengeSolvedEvent(twitterHandle, challenge, transactionHash);
-//   },
-// });
 
 console.log("Listening for ChallengeSolved events...");
