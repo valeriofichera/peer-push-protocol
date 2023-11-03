323a666018a2db0353bccab221d7255b6899ffa4
Markeljan Sokoli <Markeljan19@gmail.com>
2023-11-03 21:20:09 +0000

    backend fodler

---
+++ backend-node/dist/index.js
@@ -0,0 +1,94 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const viem_1 = require("viem");
+const chains_1 = require("viem/chains");
+dotenv_1.default.config();
+const RPC_PROVIDER_API_KEY = process.env.RPC_PROVIDER_API_KEY || '';
+const webSocketClient = (0, viem_1.createPublicClient)({
+    chain: chains_1.arbitrum,
+    transport: (0, viem_1.webSocket)(`wss://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`)
+});
+const publicClient = (0, viem_1.createPublicClient)({
+    chain: chains_1.arbitrum,
+    transport: (0, viem_1.http)(`https://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`)
+});
+// Helper function to sanitize twitter handle
+const sanitizeHandle = (twitterHandleInput) => {
+    let handle = twitterHandleInput.replace(/\s/g, '');
+    if (handle.startsWith('https://twitter.com/'))
+        handle = handle.replace('https://twitter.com/', '');
+    else if (handle.startsWith('twitter.com/'))
+        handle = handle.replace('twitter.com/', '');
+    if (!handle.startsWith('@'))
+        handle = '@' + handle;
+    if (!/^@[a-zA-Z0-9_]{1,15}$/.test(handle))
+        handle = '';
+    return handle;
+};
+// Function to handle ChallengeSolved event
+const handleChallengeSolvedEvent = async (twitterHandleInput, challenge, transactionHash) => {
+};
+// Function to get token id
+const getTokenId = async (transactionHash) => {
+    return 0;
+    // const transactionReceipt: TransactionReceipt = await publicClient.getTransactionReceipt({
+    //   hash: transactionHash,
+    // });
+    // const transferTopics = encodeEventTopics({
+    //   abi: FOUNDRY_COURSE_CONTRACT_ABI,
+    //   eventName: 'Transfer',
+    //   args: {
+    //     from: '0x0000000000000000000000000000000000000000',
+    //   }
+    // });
+    // let tokenId;
+    // for (let i = 0; i < transactionReceipt.logs.length; i++) {
+    //   if (transactionReceipt.logs[i]['topics'][0] !== transferTopics[0] || transactionReceipt.logs[i]['topics'][1] !== transferTopics[1]) {
+    //     continue;
+    //   }
+    //   const decodedEventLog = decodeEventLog({
+    //     abi: FOUNDRY_COURSE_CONTRACT_ABI,
+    //     eventName: 'Transfer',
+    //     topics: transactionReceipt.logs[i]['topics'],
+    //     data: transactionReceipt.logs[i].data,
+    //   });
+    //   tokenId = fromHex(decodedEventLog.args['tokenId'], 'number');
+    //   break;
+    // }
+    // if (!tokenId) {
+    //   console.log('No matching Transfer event found in the transaction receipt.');
+    //   return;
+    // }
+    // return tokenId;
+};
+// Function to get challenge attribute
+// const getChallengeAttribute = async (tokenId: number): Promise<string> => {
+//   const tokenUri = await publicClient.readContract({
+//     abi: FOUNDRY_COURSE_CONTRACT_ABI,
+//     address: FOUNDRY_COURSE_CONTRACT_ADDRESS,
+//     functionName: 'tokenURI',
+//     args: [tokenId],
+//   });
+//   const parsedUri = tokenUri.toString().split("base64,")[1];
+//   const jsonString = Buffer.from(parsedUri, 'base64').toString('utf-8');
+//   const jsonData = JSON.parse(jsonString);
+//   const challengeAttribute = jsonData?.attributes[0]?.trait_type;
+//   return challengeAttribute;
+// };
+// Watch for contract event
+// webSocketClient.watchContractEvent({
+//   address: FOUNDRY_COURSE_CONTRACT_ADDRESS,
+//   abi: FOUNDRY_COURSE_CONTRACT_ABI,
+//   eventName: 'ChallengeSolved',
+//   onLogs: (logs: ExpectedLog[]) => {
+//     console.log("New Challenge Solved Event!", logs);
+//     const { args: { solver, challenge, twitterHandle }, eventName, transactionHash } = logs[0];
+//     handleChallengeSolvedEvent(twitterHandle, challenge, transactionHash);
+//   },
+// });
+console.log("Listening for ChallengeSolved events...");
+//# sourceMappingURL=index.js.map
---
+++ backend-node/src/index.ts
@@ -0,0 +1,122 @@
+import dotenv from 'dotenv';
+import {
+  GetFilterLogsReturnType,
+  Log,
+  TransactionReceipt,
+  createPublicClient,
+  decodeEventLog,
+  encodeEventTopics,
+  fromHex,
+  http,
+  webSocket
+} from 'viem';
+import { arbitrum } from 'viem/chains';
+
+dotenv.config();
+
+const RPC_PROVIDER_API_KEY = process.env.RPC_PROVIDER_API_KEY || '';
+
+const webSocketClient = createPublicClient({
+  chain: arbitrum,
+  transport: webSocket(`wss://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`)
+});
+
+const publicClient = createPublicClient({
+  chain: arbitrum,
+  transport: http(`https://arb-mainnet.g.alchemy.com/v2/${RPC_PROVIDER_API_KEY}`)
+});
+
+type ExpectedLogArgs = {
+  solver: `0x${string}`;
+  challenge: `0x${string}`;
+  twitterHandle: string;
+}
+
+type ExpectedLog = {
+  args: ExpectedLogArgs;
+  eventName: string;
+  transactionHash: `0x${string}`;
+} & Log
+
+// Helper function to sanitize twitter handle
+const sanitizeHandle = (twitterHandleInput: string): string => {
+  let handle = twitterHandleInput.replace(/\s/g, '');
+  if (handle.startsWith('https://twitter.com/')) handle = handle.replace('https://twitter.com/', '');
+  else if (handle.startsWith('twitter.com/')) handle = handle.replace('twitter.com/', '');
+  if (!handle.startsWith('@')) handle = '@' + handle;
+  if (!/^@[a-zA-Z0-9_]{1,15}$/.test(handle)) handle = '';
+  return handle;
+}
+
+// Function to handle ChallengeSolved event
+const handleChallengeSolvedEvent = async (twitterHandleInput: string, challenge: `0x${string}`, transactionHash: `0x${string}`) => {
+}
+
+// Function to get token id
+const getTokenId = async (transactionHash: `0x${string}`): Promise<number> => {
+  return 0;
+  // const transactionReceipt: TransactionReceipt = await publicClient.getTransactionReceipt({
+  //   hash: transactionHash,
+  // });
+  // const transferTopics = encodeEventTopics({
+  //   abi: FOUNDRY_COURSE_CONTRACT_ABI,
+  //   eventName: 'Transfer',
+  //   args: {
+  //     from: '0x0000000000000000000000000000000000000000',
+  //   }
+  // });
+
+  // let tokenId;
+  // for (let i = 0; i < transactionReceipt.logs.length; i++) {
+  //   if (transactionReceipt.logs[i]['topics'][0] !== transferTopics[0] || transactionReceipt.logs[i]['topics'][1] !== transferTopics[1]) {
+  //     continue;
+  //   }
+
+  //   const decodedEventLog = decodeEventLog({
+  //     abi: FOUNDRY_COURSE_CONTRACT_ABI,
+  //     eventName: 'Transfer',
+  //     topics: transactionReceipt.logs[i]['topics'],
+  //     data: transactionReceipt.logs[i].data,
+  //   });
+
+  //   tokenId = fromHex(decodedEventLog.args['tokenId'], 'number');
+  //   break;
+  // }
+
+  // if (!tokenId) {
+  //   console.log('No matching Transfer event found in the transaction receipt.');
+  //   return;
+  // }
+
+  // return tokenId;
+};
+
+// Function to get challenge attribute
+// const getChallengeAttribute = async (tokenId: number): Promise<string> => {
+//   const tokenUri = await publicClient.readContract({
+//     abi: FOUNDRY_COURSE_CONTRACT_ABI,
+//     address: FOUNDRY_COURSE_CONTRACT_ADDRESS,
+//     functionName: 'tokenURI',
+//     args: [tokenId],
+//   });
+
+//   const parsedUri = tokenUri.toString().split("base64,")[1];
+//   const jsonString = Buffer.from(parsedUri, 'base64').toString('utf-8');
+//   const jsonData = JSON.parse(jsonString);
+//   const challengeAttribute = jsonData?.attributes[0]?.trait_type;
+//   return challengeAttribute;
+// };
+
+// Watch for contract event
+// webSocketClient.watchContractEvent({
+//   address: FOUNDRY_COURSE_CONTRACT_ADDRESS,
+//   abi: FOUNDRY_COURSE_CONTRACT_ABI,
+//   eventName: 'ChallengeSolved',
+//   onLogs: (logs: ExpectedLog[]) => {
+//     console.log("New Challenge Solved Event!", logs);
+//     const { args: { solver, challenge, twitterHandle }, eventName, transactionHash } = logs[0];
+//     handleChallengeSolvedEvent(twitterHandle, challenge, transactionHash);
+//   },
+// });
+
+console.log("Listening for ChallengeSolved events...");
