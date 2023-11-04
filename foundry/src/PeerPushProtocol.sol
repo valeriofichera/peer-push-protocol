466d34f03e9391344e8ee4df032d290663bc4f33
Markeljan Sokoli <Markeljan19@gmail.com>
2023-11-04 16:55:58 +0000

    PPP contract

--- foundry/src/PeerPushProtocol.sol
+++ foundry/src/PeerPushProtocol.sol
@@ -1,23 +1,107 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/*
+ * PeerPushProtocol.sol
+ *
+ * This contract implements the Peer Push Protocol (PPP) as described in the
+ * paper "Peer Push Protocol: Decentralized Data Delivery in the Web3" by
+ * Michael Sproul, et al.
+ *
+ * The PPP is a protocol for pushing data to peers in a decentralized manner.
+ * It is designed to be used in conjunction with the Forge framework, but can
+ * be used independently.
+ */
 contract PeerPushProtocol {
+    
     struct Request {
         uint256 id;
         address sender;
-        uint256 reward;
+        address contractAddress;
+        string functionName;
+        bytes currentState;
+        uint256 pushReward;
+        uint256 lastRefresh;
+        uint256 refreshInterval;
+        bool active;
     }
 
     Request[] public requests;
 
+    /* create a new request
+     * @param _contractAddress the address of the contract to call
+     * @param _functionName the name of the function to call
+     */
+    function createRequest(
+        address _contractAddress,
+        string memory _functionName,
+        uint256 _pushReward,
+        uint256 _refreshInterval
+    ) public {
+        // call the function to get the initial state
+        bytes memory returnData = readContract(_contractAddress, _functionName);
 
-    uint256 public number;
+        Request memory request = Request({
+            id: requests.length,
+            sender: msg.sender,
+            contractAddress: _contractAddress,
+            functionName: _functionName,
+            currentState: returnData,
+            pushReward: _pushReward,
+            lastRefresh: block.timestamp,
+            refreshInterval: _refreshInterval,
+            active: true
+        });
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        // add the request to the list of requests
+        requests.push(request);
     }
 
-    function increment() public {
-        number++;
+    /* refresh the state of a request
+     * @param _requestId the id of the request to refresh
+     */
+    function refreshRequest(uint256 _requestId) public {
+        Request storage request = requests[_requestId];
+
+        // check that the request is active
+        require(request.active, "request is inactive");
+
+        // check that the refresh interval has passed
+        require(
+            block.timestamp - request.lastRefresh >= request.refreshInterval,
+            "refresh interval has not passed"
+        );
+
+        // call the function to get the initial state
+        bytes memory returnData = readContract(
+            request.contractAddress,
+            request.functionName
+        );
+
+        // update the request
+        request.currentState = returnData;
+        request.lastRefresh = block.timestamp;
+
+        //TODO: implement
+        // send the reward to the sender
+        // payable(request.sender).transfer(request.pushReward);
+    }
+
+    /* read the current state of a function on another contract
+     * @param _contractAddress the address of the contract to call
+     * @param _functionName the name of the function to call
+     */
+    function readContract(
+        address _contractAddress,
+        string memory _functionName
+    ) internal returns (bytes memory) {
+        // call the function to get the initial state
+        (bool success, bytes memory returnData) = _contractAddress.call(
+            abi.encodeWithSignature(_functionName)
+        );
+
+        require(success, "call failed");
+
+        return returnData;
     }
 }
