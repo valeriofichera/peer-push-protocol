915c4067f9847216ae9237df0d168e23744d6f98
Markeljan Sokoli <Markeljan19@gmail.com>
2023-11-04 22:20:09 +0000

    Send Test notification

--- frontend/components/SendNotificationButton.tsx
+++ frontend/components/SendNotificationButton.tsx
@@ -2,14 +2,15 @@ import useSendNotification from "@/lib/hooks/useSendNotification";
 import { INotification } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 
+const APP_DOMAIN = process.env.NEXT_PUBLIC_APP_DOMAIN;
 export function SendNotificationButton() {
     const { handleSendNotification, isSending } = useSendNotification();
 
     const testNotification: INotification = {
         title: "Test Notification",
         body: "This is a test notification.",
-        icon: "/ppp.png",
-        url: "https://peer-push-protocol.vercel.app",
+        icon: `https://${APP_DOMAIN}/ppp.png`,
+        url: `https://${APP_DOMAIN}`,
         type: "055dee2f-3e83-4950-a705-0ef9ccaf832b" // type from WalletConnect Cloud
     };
 
