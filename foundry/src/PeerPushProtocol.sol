b4c8419f4ff94ef9d84487e1fd2f508e24820589
Markeljan Sokoli <Markeljan19@gmail.com>
2023-11-04 03:40:44 +0000

    foundry cleanup

--- foundry/src/PeerPushProtocol.sol
+++ foundry/src/PeerPushProtocol.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.19;
 
 contract PeerPushProtocol {
+    struct Request {
+        uint256 id;
+        address sender;
+        uint256 reward;
+    }
+
+    Request[] public requests;
+
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
